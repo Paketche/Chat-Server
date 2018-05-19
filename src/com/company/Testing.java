f7c7b457772c33500c8c5c933d359ba25f7444b1
Georgi Valchanov <g.valchanov@lancaster.ac.uk>
2018-05-19 21:36:45 +0100

    Added comment

--- src/com/company/Testing.java
+++ src/com/company/Testing.java
@@ -1,14 +1,43 @@
 package com.company;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.nio.ByteBuffer;
 
 public class Testing {
 
 
     public static void main(String[] args) {
-        ExecutorService es = Executors.newFixedThreadPool(3);
-        es.execute(()-> System.out.println("new execution"));
+
+        String someString = new String("Password. the content");
+        byte[] somestuff = new byte[10];
+
+
+        ByteBuffer buffer = ByteBuffer.allocate(1000);
+        ByteBuffer longy = ByteBuffer.allocate(Long.BYTES);
+
+
+        buffer.put(someString.getBytes());
+        buffer.position(9);
+
+        System.out.println(new String(buffer.array()));
+
+        System.out.println(new String(longy.putLong(buffer.getLong(5)).array()));
+        longy.clear();
+        System.out.println(new String(longy.putLong(buffer.getInt(1)).array()));
+        longy.clear();
+
+        buffer.reset();
+        buffer.get(somestuff);
+        System.out.println(new String(somestuff));
+
+
+        System.out.println(new String(longy.putLong(buffer.getLong(5)).array()));
+        longy.clear();
+        System.out.println(new String(longy.putLong(buffer.getInt(1)).array()));
+        longy.clear();
+
+        buffer.reset();
+        buffer.get(somestuff);
+        System.out.println(new String(somestuff));
     }
 }
 
